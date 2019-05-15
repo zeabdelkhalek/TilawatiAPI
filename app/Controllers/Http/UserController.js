76b45f98a676f352266bf079dcd15e5b77918257
Adel Abderrahmane NAMANI <ga_namani@esi.dz>
2019-05-15 16:36:57 +0100

    Refractor register and login code, add validation.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,49 +1,66 @@
 'use strict'
 const Hash = use('Hash')
 const User = use('App/Models/User')
+const { validate } = use('Validator')
 
-class UserController {
-
-
-    async register ({auth , request , response}) {
-        const body = request.post() 
-        if(!body.password) return response.status(404).send("No password !")
-        const safePassword = await Hash.make(request.input('password'))
-        const user = await User.create({ 
-                ...body ,
-                password : safePassword 
-            }).catch(err => {
-                if (err) return response.status(500).send({error : err , message : " Error on the server "})
+class UserController { 
+    async register ({request , auth , response}) {
+        const validation = await validate(request.all(), {
+            first_name: 'required|min:3|max:50',
+            last_name: 'required|min:3|max:50',
+            email: 'required|email|unique:users',
+            password: 'required|min:8|max:50|confirmed'
         })
-        if (!user) {
-            return response.status(404).send(" Error on the server ")
+        
+        if (validation.fails()){
+            return response.status(403).json(validation.messages())
         }
-        const token = await auth.generate(user)
-        return response.status(200).send({auth : true , token : token , message : "User Created Sucessfully" })
+        
+        let user = new User()
+        user.first_name = request.input('last_name')
+        user.last_name = request.input('first_name')
+        user.email = request.input('email')
+        user.password = request.input('password')
+        
+        await user.save()
+        
+        let accessToken = await auth.generate(user)
+        
+        return response.status(200).json({
+            "user": user,
+            "accessToken": accessToken
+        })
     }
-
-
+      
     async login ({auth , request , response}) {
-        const body = request.post() 
-        if(!body) return response.status(404).send("No data !")
-
-        const user = await User.findBy('email', body.email)
-        
-        .catch(err => {
-                if (err) return response.status(500).send({error : err , message : " Error on the server "})
+        const validation = await validate(request.all(), {
+            email: 'required',
+            password: 'required'
         })
-        
-        if ( !user ) {
-            return response.status(404).send(" No user found ")
+
+        if (validation.fails()) {
+            return response.status(403).json(validation.messages())
         }
-        const isPasswordValid = await Hash.verify( user.password , body.password  )
-        
-        if(!isPasswordValid) return response.status(401).send({auth : false , token : null , message : " Password Incorrect " })
 
-        const token = await auth.generate(user)
-        return response.status(200).send({auth : true , token : token , message : "User Login Sucessfully" })
+        const email = request.input('email')
+        const password = request.input('password')
+        try {
+            if (await auth.attempt(email, password)) {
+                let user = await User.findBy('email', email)
+                let accessToken = await auth.generate(user)
+                return response.status(200).json({
+                    'user': user,
+                    'access_token': accessToken
+                })
+            }
+        }
+        catch (e) {
+            return response.status(403).json([{
+                message: 'Email and password does not match with any account !'
+            }])
+        } 
     }
-
+    
 }
 
 module.exports = UserController
